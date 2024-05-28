6048d5c1385e04bb06c09ef3e7c6daa948dbec09
yuto <yuto.uehara@mail.rooxim.com>
2024-05-28 19:13:25 +0900

    add 404.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath } from 'node:url';
+import { join, parse, resolve } from "node:path";
 import imagePresets, { widthPreset } from 'vite-plugin-image-presets'
 
 // https://vitejs.dev/config/
@@ -43,5 +44,26 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: './'
-})
+  base: './',
+  build: {
+    rollupOptions: {
+      input: entryPoints(
+        "index.html",
+        "404.html",
+      ),
+    },
+  }
+});
+
+
+function entryPoints(...paths) {
+  const entries = paths.map(parse).map(entry => {
+    const { dir, base, name, ext } = entry;
+    const key = join(dir, name);
+    const path = resolve(__dirname, dir, base);
+    return [key, path];
+  });
+
+  const config = Object.fromEntries(entries);
+  return config;
+}
