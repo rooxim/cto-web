4ced8a41c716d96b44a1fda7677313c0c5d744f1
Yuto Uehara <yuto.uehara@mail.rooxim.com>
2024-06-12 17:10:36 +0900

    update txt and img.

--- src/components/prices.vue
+++ src/components/prices.vue
@@ -14,11 +14,6 @@
                           publishable-key="pk_test_51KtqDVBcykgBJMyJBEMfGm2vma6X2RS3hqQ5vmbHG97UpvjCzSL8yy75wg8XdFDbWeg8uJhnSGfIpcGm9r0M4NuO00OVoqRmgu">
     </stripe-pricing-table>
 
-
-    スタンダード　9800円
-    プレミアム　528000円
-
-
     <div class="note">
       <p><span class="emphasis">スタンダードプランプラン</span>は、<span class="emphasis">フリーランス</span>や<span class="emphasis">個人事業主</span>の方におすすめのプランです。</p>
       <p><span class="emphasis">プレミアムプラン</span>は、<span class="emphasis">その他法人</span>の方におすすめのプランです。</p>
